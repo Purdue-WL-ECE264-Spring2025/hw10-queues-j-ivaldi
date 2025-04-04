1c23eb285a9d0dba543f757eb7e26b0c2a865f69
j-ivaldi <jivaldi@purdue.edu>
2025-04-03 21:46:07 -0400

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,83 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { return NULL; 
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  
+  struct list_node *new_node = (struct list_node *)malloc(sizeof(struct list_node));
+  new_node->value = value;
+  new_node->next = list->head;
+  list->head = new_node;
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+void insert_at_tail(struct linked_list *list, size_t value) {
+  
+  struct list_node *new_node = (struct list_node *)malloc(sizeof(struct list_node));
+  new_node->value = value;
 
-void free_list(struct linked_list list) {}
+  if (list->head == NULL) {
+    list->head = new_node;
+    new_node->next = NULL;
+    return;
+  }
+
+
+  struct list_node *traverse = list->head;
+  while(list->head != NULL){
+    traverse = traverse->next;
+  }
+  new_node->value = value;
+  traverse->next = new_node;
+  new_node->next = NULL;
+  
+}
+
+size_t remove_from_head(struct linked_list *list) { 
+  if(list == NULL){
+    return 0;
+  }
+  struct list_node *del = list->head;
+  size_t retval = del->value;
+  list->head = del->next;
+  free(del);
+  
+return retval; 
+}
+
+size_t remove_from_tail(struct linked_list *list) { 
+  
+  if(list == NULL){
+    return 0;
+  }
+
+  struct list_node *traverse = list->head;
+  struct list_node *trav = NULL;
+
+  while(list->head != NULL){
+    trav = traverse;
+    traverse = traverse->next;
+  }
+  size_t retval = traverse->value;
+  free(traverse);
+  trav->next = NULL;
+  
+  
+return retval; }
+
+void free_list(struct linked_list list) {
+  struct list_node *temp;
+  while(temp->next != NULL){
+    temp = list.head;
+    list.head = list.head->next;
+    free(temp);
+  }
+
+
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
