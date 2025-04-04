571219fc767fe6c52da7edd94061927ab0fe1b28
j-ivaldi <jivaldi@purdue.edu>
2025-04-04 15:11:46 -0400

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -1,3 +1,10 @@
+#include "linked_list.h"
+
+#include <stdlib.h>
+
+struct list_node *new_node(size_t value) { return NULL; 
+}
+
 void insert_at_head(struct linked_list *list, size_t value) {
   
   struct list_node *new_node = (struct list_node *)malloc(sizeof(struct list_node));
@@ -71,4 +78,13 @@ void free_list(struct linked_list list) {
   }
 
 
+}
+
+// Utility function to help you debugging, do not modify
+void dump_list(FILE *fp, struct linked_list list) {
+  fprintf(fp, "[ ");
+  for (struct list_node *cur = list.head; cur != NULL; cur = cur->next) {
+    fprintf(fp, "%zu ", cur->value);
+  }
+  fprintf(fp, "]\n");
 }
